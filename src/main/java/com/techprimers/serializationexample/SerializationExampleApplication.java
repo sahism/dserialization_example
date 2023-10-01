fefe3221665904a175dcc2f1178670aaaa2b5194
Michael <dahis.michael@gmail.com>
2023-10-01 14:40:09 +0300

    Add files via upload

---
+++ src/main/java/com/techprimers/serializationexample/SerializationExampleApplication.java
@@ -0,0 +1,12 @@
+package com.techprimers.serializationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerializationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerializationExampleApplication.class, args);
+	}
+}
